547ca29a27b6a9cf94943499a39a0f1e51e08f63
Boutin Florent <fboutin76@gmail.com>
2016-03-21 16:53:52 +0100

    Create exos.js

---
+++ exos.js
@@ -0,0 +1,66 @@
+// ---------- //
+// JAVASCRIPT //
+// ---------- //
+
+/* 
+1. Définir une variable de type String.
+2. Ajouter à la variable, une chaine de caractère.
+3. Afficher dans la console la valeur de notre variable.
+*/
+
+/*
+1. Définir une variable de type String, et lui ajouter une chaine de caractère.
+2. Créer une fonction qui permet d'afficher dans la console, la valeur de notre variable.
+*/
+
+/*
+1. Définir une variable de type Integer.
+2. Créer une fonction, qui ajoute à la variable la valeur passée en argument de cette fonction.
+3. Créer une autre fonction qui permet d'afficher dans une alert, la valeur de notre variable.
+*/
+
+/*
+1. Définir une variable de type Array.
+2. Créer une fonction, qui vient ajouter, à la fin de notre variable tableau, la valeur passé en argument.
+3. Créer une autre fonction, qui permet d'affiche la valeur du tableau présente à l'index passé en arguement de cette fonction.
+*/
+
+/*
+1. Définir une variable de type Array.
+2. Lui ajouter les valeurs suivante : "rouge", "bleu", "jaune", "violet", "vert".
+3. Créer une fonction qui, une fois appelé, va écrire dans la console, chacune des valeurs du tableau.
+*/
+
+// ----------------- //
+// JAVASCRIPT & HTML //
+// ----------------- //
+/*
+1. Créer un document HTML, contenant un titre et un paragraphe.
+2. Utiliser le JS pour afficher dans la console, le contenu du titre, puis le contenu du paragraphe.
+*/
+
+/*
+1. Créer un document HTML, contenant un titre et un paragraphe.
+2. Modifier en JS, le texte du titre puis celui du paragraphe.
+*/
+
+/*
+1. Créer un document HTML, contenant un titre et un paragraphe.
+2. Ajouter à la suite du document un formulaire contenant un input de type text, et une zone de texte (textarea).
+3. Faire en sorte que lors de la soumission du formulaire, le contenu du titre et du paragraphe soit mit à jours avec les valeurs du formulaire.
+4. Quand c'est bon, ajouter un color picker au formulaire qui vien changer la couleur de background de la page HTML.
+*/
+
+/*
+1. Reprendre l'exercice précédent.
+2. Retirer le bouton de soumission du formulaire.
+3. Faire en sorte que le contenus soit changer en même temps que l'on écrit dans le formulaire.
+*/
+
+// ----------------- //
+// JQUERY & HTML //
+// ----------------- //
+/*
+1. Reprendre l'exercice précédent.
+2. Remplacer toutes les instructions JavaScript par des instruction jQuery.
+*/
